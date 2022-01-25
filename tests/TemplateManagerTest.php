896201f2c55c0b2db83179a859855633e6eacfe9
Allouche Mickael <Mickael.allouche@gmail.com>
2022-01-25 17:51:32 +0100

    add mocks :D

--- tests/TemplateManagerTest.php
+++ tests/TemplateManagerTest.php
@@ -4,23 +4,48 @@ declare(strict_types=1);
 
 use App\App\Service\ApplicationContext;
 use App\App\Service\TemplateManager;
+use App\Domain\Destination;
 use App\Domain\DestinationRepository;
+use App\Domain\Quote;
 use App\Domain\QuoteRepository;
+use App\Domain\Site;
+use App\Domain\SiteRepository;
 use App\Domain\Template;
+use App\Domain\User;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 
-$container = require_once __DIR__.'/../app/bootstrap.php';
-
-class TemplateManagerTest extends \PHPUnit\Framework\TestCase
+class TemplateManagerTest extends TestCase
 {
-    private $container;
+    private QuoteRepository|MockObject $quoteRepository;
+    private DestinationRepository|MockObject $destinationRepository;
+    private ApplicationContext|MockObject $applicationContext;
+    private SiteRepository $siteRepository;
 
     /**
      * Init the mocks.
      */
     public function setUp(): void
     {
-        global $container;
-        $this->container = $container;
+        $this->applicationContext = $this
+            ->getMockBuilder(ApplicationContext::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->quoteRepository = $this
+            ->getMockBuilder(QuoteRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->destinationRepository = $this
+            ->getMockBuilder(DestinationRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->siteRepository = $this
+            ->getMockBuilder(SiteRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
     }
 
     /**
@@ -30,17 +55,38 @@ class TemplateManagerTest extends \PHPUnit\Framework\TestCase
     {
     }
 
-    /**
-     * @test
-     */
-    public function test()
+    public function testGetTemplateComputed()
     {
-        $faker = Faker\Factory::create();
+        $generator = Faker\Factory::create();
 
-        $quote = $this->container->get(QuoteRepository::class)->getById($faker->randomNumber());
-        $expectedDestination = $this->container->get(DestinationRepository::class)->getById($quote->destinationId);
-        $expectedUser = $this->container->get(ApplicationContext::class)->getCurrentUser();
-        $templateManager = $this->container->get(TemplateManager::class);
+        $quote = new Quote(
+            $generator->numberBetween(),
+            $generator->numberBetween(1, 10),
+            $generator->numberBetween(1, 200),
+            $generator->dateTime()
+        );
+
+        $destination = new Destination(
+            $generator->numberBetween(),
+            $generator->country,
+            'en',
+            $generator->slug()
+        );
+
+        $site = new Site($generator->randomNumber(), $generator->url);
+        $user = new User($generator->randomNumber(), $generator->firstName, $generator->lastName, $generator->email);
+
+        $this->applicationContext->method('getCurrentUser')->willReturn($user);
+        $this->quoteRepository->method('getById')->willReturn($quote);
+        $this->destinationRepository->method('getById')->willReturn($destination);
+        $this->siteRepository->method('getById')->willReturn($site);
+
+        $templateManager = new TemplateManager(
+            $this->applicationContext,
+            $this->quoteRepository,
+            $this->destinationRepository,
+            $this->siteRepository
+        );
 
         $template = new Template(
             1,
@@ -59,16 +105,14 @@ www.evaneos.com
 
         $message = $templateManager->getTemplateComputed(
             $template,
-            [
-                'quote' => $quote,
-            ]
+            ['quote' => $quote]
         );
 
-        $this->assertEquals('Votre voyage avec une agence locale '.$expectedDestination->countryName, $message->subject);
+        $this->assertEquals('Votre voyage avec une agence locale '.$destination->countryName, $message->subject);
         $this->assertEquals('
-Bonjour '.$expectedUser->firstname.",
+Bonjour '.$user->firstname.",
 
-Merci d'avoir contacté un agent local pour votre voyage ".$expectedDestination->countryName.".
+Merci d'avoir contacté un agent local pour votre voyage ".$destination->countryName.".
 
 Bien cordialement,
 
